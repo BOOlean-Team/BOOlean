dd02ad7041cc711fed79dd59d8bd41a692b84958
Hue <huexkoh@gmail.com>
2024-01-21 04:17:21 +0800

    the ghost moves fine now

--- src/click.py
+++ src/click.py
@@ -1,18 +1,20 @@
 import random
-from PyQt5.QtCore import Qt, QTimer, QPointF, QEasingCurve, QPropertyAnimation
+from PyQt5.QtCore import Qt, QPointF, QEasingCurve, QPropertyAnimation, QTimer
+from PyQt5.QtWidgets import QLabel
+
 class ClickHandler:
     def __init__(self, widget):
         self.widget = widget
         self.offset = QPointF()
-        self.move_timer = QTimer(self.widget)
-        self.move_timer.timeout.connect(self.move_pet_smoothly)
-        self.move_timer.start(3000)  # Adjust the interval as needed (milliseconds)
-
         self.animation = QPropertyAnimation(self.widget, b'pos', self.widget)
         self.animation.setEasingCurve(QEasingCurve.Linear)
+        self.move_timer = QTimer(self.widget)
+        self.move_timer.timeout.connect(self.move_pet_smoothly)
+        self.move_timer.start(3500)  # Adjust the interval as needed (milliseconds)
 
         self.widget.mousePressEvent = self.start_drag
         self.widget.mouseMoveEvent = self.drag_pet
+        self.widget.mouseReleaseEvent = self.end_drag  # Added mouse release event
 
     def move_pet_smoothly(self):
         screen_rect = self.widget.screen().availableGeometry()
@@ -23,27 +25,17 @@ class ClickHandler:
 
         self.animation.setStartValue(self.widget.pos())
         self.animation.setEndValue(QPointF(target_x, target_y))
-        self.animation.setDuration(5000)  # Adjust the duration as needed (milliseconds)
+        self.animation.setDuration(1300)  # Adjust the duration as needed (milliseconds)
         self.animation.start()
 
     def start_drag(self, event):
         if event.buttons() == Qt.LeftButton:
             self.offset = event.pos()
+            self.animation.pause()  # Pause the animation when dragging starts
 
     def drag_pet(self, event):
         if event.buttons() == Qt.LeftButton:
             self.widget.move(event.globalPos() - self.offset)
 
-
-#ANIMATION_DELAY = 150
-
-#click_counter = 0
-
-#def on_click_event(pos, event, window, pet_widget, angry_change):
-
-#    global click_counter
-
-    #print(f"Click Count: {click_counter}")
-#    window.after(ANIMATION_DELAY, angry_change, pos, pet_widget, click_counter)
-#    click_counter += 1
-
+    def end_drag(self, event):
+        self.animation.resume()  # Resume the animation when dragging ends
