1f6fb520f619cbd54637dd8a7d73ac0c832e3016
kkaiyu <127333335+kkaiyu@users.noreply.github.com>
2024-01-21 10:33:36 +0800

    angry drag

--- src/click.py
+++ src/click.py
@@ -1,14 +1,17 @@
-import random
-from PyQt5.QtCore import Qt, QPointF, QEasingCurve, QPropertyAnimation, QTimer
+from PyQt5.QtCore import Qt, QPointF
 from PyQt5.QtWidgets import QLabel
 from chatgpt import NewChatGPTWindow
 from freemovement import MovementHandler
 
 
 class ClickHandler:
-    def __init__(self, widget):
+    def __init__(self, main_window, widget, default_gif, angry_gif):
+        self.main_window = main_window
         self.widget = widget
         self.offset = QPointF()
+
+        self.default_gif = default_gif
+        self.angry_gif = angry_gif
 
         self.free_movement_handler = MovementHandler(self.widget)
 
@@ -31,11 +34,40 @@ class ClickHandler:
             self.offset = event.pos()
             # Pause the animation when dragging starts
             self.free_movement_handler.animation.pause()
+            # Display angry GIF
+            self.update_angry_state(True)
 
     def drag_pet(self, event):
         if event.buttons() == Qt.LeftButton:
             self.widget.move(event.globalPos() - self.offset)
+            self.update_dragging_state()
 
     def end_drag(self, event):
         # Resume the animation when dragging ends
         self.free_movement_handler.animation.resume()
+        # Display default GIF
+        self.update_angry_state(False)
+    
+    def update_dragging_state(self):
+        self.main_window.label.setMovie(self.angry_gif)
+        self.angry_gif.start()
+
+    def update_angry_state(self, is_angry):
+        gif =  self.angry_gif if is_angry else self.default_gif
+        self.main_window.label.setMovie(gif)
+        gif.start()
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    main_window = MainWindow()
+
+    click_handler = ClickHandler(main_window, main_window.w, 
+                        main_window.default, main_window.angry)
+    main_window.w.mousePressEvent = click_handler.handle_mouse_press
+    main_window.w.mouseMoveEvent = click_handler.drag_pet
+    main_window.w.mouseReleaseEvent = click_handler.end_drag
+    sys.exit(app.exec())
+
+if __name__ == '__main__':
+    main()
+
--- src/main.py
+++ src/main.py
@@ -13,6 +13,7 @@ WINDOW_SIZE = 100
 DEFAULT_ANIMATION = 1
 WALK_RIGHT_ANIMATION = 2
 WALK_LEFT_ANIMATION = 3
+ANGRY_ANIMATION = 4
 
 PET_SIZE = 100
 
@@ -21,8 +22,8 @@ current_state = random.randrange(1, 4, 1)
 
 
 class MainWindow(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, parent=None):
+        super().__init__(parent)
 
         self.default, self.walk_right, self.walk_left, self.angry = load_images()
         self.default.setScaledSize(QtCore.QSize(PET_SIZE, PET_SIZE))
@@ -42,11 +43,6 @@ class MainWindow(QtWidgets.QWidget):
                               QtCore.Qt.FramelessWindowHint)
         self.w.setAttribute(QtCore.Qt.WA_TranslucentBackground)
         self.w.show()
-
-        click_handler = ClickHandler(self.w)
-        self.w.mousePressEvent = click_handler.handle_mouse_press
-        self.w.mouseMoveEvent = click_handler.drag_pet
-        self.w.mouseReleaseEvent = click_handler.end_drag
 
         self.label.setMovie(self.default)
         self.default.start()
@@ -69,6 +65,8 @@ class MainWindow(QtWidgets.QWidget):
             gif = self.walk_right
         elif (current_state == WALK_LEFT_ANIMATION):
             gif = self.walk_left
+        elif (current_state == ANGRY_ANIMATION):
+            gif = self.angry
 
         self.label.setMovie(gif)
         gif.start()
@@ -79,7 +77,13 @@ GHOST_IDLE = 'assets/idle.gif'
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
-    MainWindow()
+    main_window = MainWindow()
+
+    click_handler = ClickHandler(main_window, main_window.w, 
+                        main_window.default, main_window.angry)
+    main_window.w.mousePressEvent = click_handler.handle_mouse_press
+    main_window.w.mouseMoveEvent = click_handler.drag_pet
+    main_window.w.mouseReleaseEvent = click_handler.end_drag
     sys.exit(app.exec())
 
 
