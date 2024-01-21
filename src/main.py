4635c151d41d92a95128e3dd5b7398ab146e758b
Hue <huexkoh@gmail.com>
2024-01-21 09:13:56 +0800

    separated click into ClickHandler and MovementHandler

--- src/main.py
+++ src/main.py
@@ -5,27 +5,33 @@ from PyQt5.QtCore import QRect, QTimer
 from PyQt5 import QtCore
 from click import ClickHandler
 
+GHOST_IDLE = 'assets/idle.gif'
+
 def main():
     app = QApplication(sys.argv)
 
+    # creates the widget base (UI object)
     w = QWidget()
     w.resize(250, 200)
     w.move(300, 300)
 
-    gif = QMovie('assets/idle.gif')
-    gif.setScaledSize(QtCore.QSize(100, 100))
+    # load in the default ghost and set its sizing etc
+    idle_gif = QMovie(GHOST_IDLE)
+    idle_gif.setScaledSize(QtCore.QSize(100, 100))
 
     label = QLabel(w)
     label.setGeometry(QRect(25, 25, 200, 200))
-    label.setMovie(gif)
+    label.setMovie(idle_gif)
     label.resize(100, 100)
-    gif.start()
+    idle_gif.start()
 
+    # makes the ghost always on top window
     w.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint |
                      QtCore.Qt.FramelessWindowHint)
     w.setAttribute(QtCore.Qt.WA_TranslucentBackground)
     w.show()
 
+    # replace these attributes of w with the defined functions from ClickHandler
     click_handler = ClickHandler(w)
     w.mousePressEvent = click_handler.start_drag
     w.mouseMoveEvent = click_handler.drag_pet
